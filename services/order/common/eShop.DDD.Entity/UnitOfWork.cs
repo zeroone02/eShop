2bfd8a9852f3fc8eae64ddac53a7312e7a1e2d57
artur <zeroone02@mail.ru>
2023-07-26 21:13:16 +0300

    Создал структуру других сервисов

---
+++ services/order/common/eShop.DDD.Entity/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿namespace eShop.DDD.Entity;
+public class UnitOfWork
+{
+    private IEfCoreDbContext _db;
+
+    public UnitOfWork(IEfCoreDbContext db)
+    {
+        db = _db;
+    }
+    public void SaveChangesAsync()
+    {
+        _db.SaveChangesAsync();
+    }
+}
---
+++ services/shoppingCart/common/eShop.DDD.Entity/Entities/IEntity.cs
@@ -0,0 +1,5 @@
+﻿namespace eShop.DDD.Entity;
+public interface IEntity<TKey>
+{
+    TKey Id { get; set; }
+}
