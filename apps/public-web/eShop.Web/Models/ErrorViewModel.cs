60a1d710cb7f965757b2257f7aed245edc7791ff
artur <zeroone02@mail.ru>
2023-08-01 14:35:46 +0300

    WIP

--- apps/public-web/eShop.Web/Models/ErrorViewModel.cs
+++ apps/public-web/eShop.Web/Models/ErrorViewModel.cs
@@ -3,6 +3,6 @@ namespace eShop.Web.Models;
 public class ErrorViewModel
 {
     public string? RequestId { get; set; }
-
+    IHttpClientFactory
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
--- apps/public-web/eShop.WebService/Service/BaseService.cs
+++ apps/public-web/eShop.WebService/Service/BaseService.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using eShop.DDD.Application.Contracts;
+using eShop.Web.Application.Contracts;
+using eShop.WebUtility;
+using System.Net.Http;
 
-namespace eShop.Web.Application.Service;
-internal class BaseService
+namespace eShop.Web.Application;
+public class BaseService<T> : IBaseService<T>
 {
+    private readonly IHttpClientFactory _httpClientFactory;
+    public BaseService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<ResponseDto<T>?> SendAsync(RequestDto<T> requestDto)
+    {
+        HttpClient client = _httpClientFactory.CreateClient();
+    }
 }
