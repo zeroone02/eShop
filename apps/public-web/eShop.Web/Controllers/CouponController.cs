4b85403d55af741418558da1c5c973aafe86d7b5
artur <zeroone02@mail.ru>
2023-08-04 19:11:35 +0300

    Добавил CouponCreate

---
+++ apps/public-web/eShop.Web/Controllers/CouponController.cs
@@ -0,0 +1,45 @@
+﻿using eShop.DDD.Application.Contracts;
+using eShop.Web.Application.Contracts;
+using eShop.Web.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace eShop.Web.Controllers;
+public class CouponController : Controller
+{
+    private readonly ICouponService _couponService;
+
+    public CouponController(ICouponService couponService)
+    {
+        _couponService = couponService;
+    }
+
+    public async  Task<IActionResult> CouponIndex()
+    {
+        List<CouponDto>? list = new();
+        ResponseDto? response = await _couponService.GetAllCouponsAsync();
+        if (response != null && response.IsSuccess)
+        {
+            list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+        }
+        return View(list);
+    }
+	public async Task<IActionResult> CouponCreate()
+	{
+		return View();
+	}
+	[HttpPost]
+	public async Task<IActionResult> CouponCreate(CouponDto model)
+	{
+		if (ModelState.IsValid)
+        {
+			ResponseDto? response = await _couponService.CreateCouponsAsync(model);
+			if (response != null && response.IsSuccess)
+			{
+                return RedirectToAction(nameof(CouponIndex));
+			}
+		}
+        return View(model);
+	}
+}
