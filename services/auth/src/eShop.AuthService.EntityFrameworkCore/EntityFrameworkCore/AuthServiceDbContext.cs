8779f9a542c1869d73276435dc06f4430b007327
artur <zeroone02@mail.ru>
2023-08-05 15:53:24 +0300

    Добавил базу данных в auth

---
+++ services/auth/src/eShop.AuthService.EntityFrameworkCore/EntityFrameworkCore/AuthServiceDbContext.cs
@@ -0,0 +1,18 @@
+﻿using eShop.DDD.Entity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShop.AuthService.EntityFrameworkCore;
+public class AuthServiceDbContext : IdentityDbContext<IdentityUser>, IEfCoreDbContext
+{
+    public AuthServiceDbContext(DbContextOptions<AuthServiceDbContext> options)
+         : base(options)
+    {
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
