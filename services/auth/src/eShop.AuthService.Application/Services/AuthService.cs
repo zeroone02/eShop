a44db6c9a97d1a9c27b430e9a65dd337fc37f62e
artur <zeroone02@mail.ru>
2023-08-06 20:31:52 +0300

    WIP

--- services/auth/src/eShop.AuthService.Application/Services/AuthService.cs
+++ services/auth/src/eShop.AuthService.Application/Services/AuthService.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using eShop.AuthService.Application.Contracts;
+using eShop.AuthService.Domain;
+using eShop.AuthService.EntityFrameworkCore;
+using eShop.DDD.Entity;
+using Microsoft.AspNetCore.Identity;
 
-namespace eShop.AuthService.Application.Services;
-internal class AuthService
+namespace eShop.AuthService.Application;
+public class AuthService : IAuthService
 {
+    private readonly AuthServiceDbContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    public AuthService(AuthServiceDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        _db = db;
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+
+    public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+    {
+        throw new NotImplementedException();
+    }
 }
