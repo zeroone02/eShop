eaf4d71c730e63a7c68e0432e2cb9f9db0a3125e
artur <zeroone02@mail.ru>
2023-08-11 20:56:39 +0300

    WIP

--- apps/public-web/eShop.Web/Controllers/AuthController.cs
+++ apps/public-web/eShop.Web/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
-﻿using eShop.Web.Application.Contracts;
+﻿using eShop.DDD.Application.Contracts;
+using eShop.Web.Application.Contracts;
 using eShop.Web.Domain;
+using eShop.Web.Domain.Domain.Shared;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -23,8 +25,39 @@ public class AuthController : Controller
     [HttpGet]
     public IActionResult Register()
     {
-        var roleList = new List<SelectList>();
+        var roleList = new List<SelectListItem>()
+        {
+            new SelectListItem() {Text=SD.RoleAdmin,Value=SD.RoleAdmin},
+            new SelectListItem() {Text=SD.RoleCustomer,Value=SD.RoleCustomer},
+        };
+        ViewBag.RoleList = roleList;
         return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Register(RegistrationRequestDto obj)
+    {
+        ResponseDto result = await _authService.RegisterAsync(obj);
+        ResponseDto assignRole;
+        if (result != null && result.IsSuccess)
+        {
+            if (string.IsNullOrEmpty( obj.Role))
+            {
+                obj.Role = SD.RoleCustomer;
+            }
+            assignRole = await _authService.AssignRoleAsync(obj);
+            if (assignRole != null && assignRole.IsSuccess)
+            {
+                TempData["success"] = "Registration Successful";
+                return RedirectToAction(nameof(Login));
+            }
+        }
+        var roleList = new List<SelectListItem>()
+        {
+            new SelectListItem() {Text=SD.RoleAdmin,Value=SD.RoleAdmin},
+            new SelectListItem() {Text=SD.RoleCustomer,Value=SD.RoleCustomer},
+        };
+        ViewBag.RoleList = roleList;
+        return View(obj);
     }
     [HttpGet]
     public IActionResult Logout()
