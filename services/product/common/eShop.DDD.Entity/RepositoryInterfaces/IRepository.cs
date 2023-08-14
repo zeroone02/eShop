527c1f22f8b2d29e5d45df9b14459bff8b9ba253
artur <zeroone02@mail.ru>
2023-08-14 19:50:00 +0300

    wip

--- services/product/common/eShop.DDD.Entity/RepositoryInterfaces/IRepository.cs
+++ services/product/common/eShop.DDD.Entity/RepositoryInterfaces/IRepository.cs
@@ -5,10 +5,10 @@ namespace eShop.DDD.Entity;
 public interface IRepository<TEntity, TKey> where TEntity : Entity<TKey>
 {
     Task<TEntity> InsertAsync(TEntity entity);
-    Task DeleteAsync(TEntity entity);
+    Task Delete(TEntity entity);
     IQueryable<TEntity> GetQueryable();
     Task<TEntity> GetAsync(TKey id);
     Task<List<TEntity>> GetListAsync();
     Task<TEntity> UpdateAsync(TEntity entity);
-    IQueryable<TEntity> WithDetailsAsync(params Expression<Func<TEntity, object>>[] func);
+    IQueryable<TEntity> WithDetails(params Expression<Func<TEntity, object>>[] func);
 }
