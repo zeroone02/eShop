74ca31a881de911f46e9bb28168f57d2c030e6c0
artur <zeroone02@mail.ru>
2023-08-14 20:15:53 +0300

    WIP

---
+++ services/product/src/eShop.ProductService.EntityFrameworkCore/EntityFrameworkCore/ProductServiceDbContext.cs
@@ -0,0 +1,17 @@
+﻿using eShop.DDD.Entity;
+using Microsoft.EntityFrameworkCore;
+using eShop.ProductService.Domain;
+namespace eShop.ProductService.EntityFrameworkCore;
+public class ProductServiceDbContext : DbContext, IEfCoreDbContext
+{
+   public ProductServiceDbContext(DbContextOptions<ProductServiceDbContext> options )
+        : base(options)
+    {
+
+    }
+    public DbSet<Product> Products { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
