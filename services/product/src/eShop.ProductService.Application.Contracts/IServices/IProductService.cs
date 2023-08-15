53047662f2eb8f28e47a88247f68d866368dbf6a
artur <zeroone02@mail.ru>
2023-08-15 18:30:20 +0300

    WIP

---
+++ services/product/src/eShop.ProductService.Application.Contracts/IServices/IProductService.cs
@@ -0,0 +1,12 @@
+﻿
+using eShop.ProductService.Domain;
+
+namespace eShop.ProductService.Application.Contracts;
+public interface IProductService
+{
+    public Task<IEnumerable<ProductDto>> GetListAsync();
+    public Task<ProductDto> GetAsync(Guid id);
+    public Task<Product> AddAsync(ProductDto ProductDto);
+    public Task<Product> DeleteAsync(Guid id);
+    public Task<ProductDto> UpdateAsync(ProductDto ProductDto);
+}
---
+++ services/product/src/eShop.ProductService.Application/Services/ProductService.cs
@@ -0,0 +1,31 @@
+﻿using eShop.ProductService.Application.Contracts;
+using eShop.ProductService.Domain;
+
+namespace eShop.ProductService.Application;
+public class ProductService : IProductService
+{
+    public Task<Product> AddAsync(ProductDto ProductDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductDto> GetAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<ProductDto>> GetListAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductDto> UpdateAsync(ProductDto ProductDto)
+    {
+        throw new NotImplementedException();
+    }
+}
