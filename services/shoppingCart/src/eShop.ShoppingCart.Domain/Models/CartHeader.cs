363db59b5f585cd55f4c90fdb514705bd8106066
artur <zeroone02@mail.ru>
2023-08-19 18:12:12 +0300

    wip

--- services/shoppingCart/src/eShop.ShoppingCart.Domain/Models/CartHeader.cs
+++ services/shoppingCart/src/eShop.ShoppingCart.Domain/Models/CartHeader.cs
@@ -1,4 +1,14 @@
-﻿namespace eShop.ShoppingCartService.Domain;
-public class CartHeader
+﻿using eShop.DDD.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eShop.ShoppingCartService.Domain;
+public class CartHeader : Entity<Guid>
 {
+    public Guid? UserId { get; set; }
+    public string? CouponCode { get; set; }
+
+    [NotMapped]
+    public double Discount { get; set; }
+    [NotMapped]
+    public double CartTotal { get; set; }
 }
